e8e7a06a3990ce857eb057ff09dd85d105f6f138
FELIPE VASEL <felipe_vasel@estudante.sesisenai.org.br>
2024-10-24 20:53:44 -0300

    Criação script migration

--- src/db/migration/V1__CREATE_TABLES.js
+++ src/db/migration/V1__CREATE_TABLES.js
@@ -1,31 +1,42 @@
 const conexao = require ("../conexao/conexao.js");
 
 const criarTabelaLoja = "CREATE TABLE IF NOT EXISTS LOJA("
-                            + "ID INT PRIMARY KEY AUTO_INCREMENT,"
-                            + "NOME VARCHAR(50),"
-                            + "ENDERECO VARCHAR(100)"
+                            + "`ID` INT PRIMARY KEY AUTO_INCREMENT,"
+                            + "`NOME` VARCHAR(50),"
+                            + "`ENDERECO` VARCHAR(100)"
                         +")";
 
-conexao.query(criarTabelaAluno, (erro, resultado) => {
+conexao.query(criarTabelaLoja, (erro, resultado) => {
     if (erro) {
         throw new Error(erro);
     }
 });
 
 const criarTabelaProduto = "CREATE TABLE IF NOT EXISTS PRODUTO("
-                                + "ID INT PRIMARY KEY AUTO_INCREMENT,"
-                                + "NOME VARCHAR(50),"
-                                + "DESCRICAO VARCHAR(200),"
-                                + "PRECO FLOAT,"
-                                + "QUANTIDADE INT,"
-                                + "LOJA_ID INT REFERENCES LOJA(ID)"
+                                + "`ID` INT PRIMARY KEY AUTO_INCREMENT,"
+                                + "`NOME` VARCHAR(50),"
+                                + "`DESCRICAO` VARCHAR(200),"
+                                + "`PRECO` FLOAT,"
+                                + "`QUANTIDADE` INT,"
+                                + "`LOJA_ID` INT REFERENCES LOJA(ID)"
                             +")";
 
-const criarTabelaMovimentacao = "CREATE TABLE IF NOT EXISTS PRODUTO("
-                            + "ID INT PRIMARY KEY AUTO_INCREMENT,"
-                            + "NOME VARCHAR(50),"
-                            + "DESCRICAO VARCHAR(200),"
-                            + "PRECO FLOAT,"
-                            + "QUANTIDADE INT,"
-                            + "LOJA_ID INT REFERENCES LOJA(ID)"
-                        +")";
+conexao.query(criarTabelaProduto, (erro, resultado) => {
+    if (erro) {
+        throw new Error(erro);
+    }
+});
+
+const criarTabelaMovimentacao = "CREATE TABLE IF NOT EXISTS MOVIMENTACAO("
+                                    + "`ID` INT PRIMARY KEY AUTO_INCREMENT,"
+                                    + "`TIPO` ENUM('ENTRADA', 'SAIDA'),"
+                                    + "`QUANTIDADE` INT,"
+                                    + "`DATA` DATE,"
+                                    + "`PRODUTO_ID` INT REFERENCES PRODUTO(ID)"
+                                +")";
+
+conexao.query(criarTabelaMovimentacao, (erro, resultado) => {
+    if (erro) {
+        throw new Error(erro);
+    }
+});
